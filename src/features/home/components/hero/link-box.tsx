23efc32c23f41e444d64ae3ed684f7479d1d61a3
comarvelsystems <tkahmedkamal@marvel.com.sa>
2025-03-03 04:26:42 +0200

    Refactor link box

--- src/features/home/components/hero/link-box.tsx
+++ src/features/home/components/hero/link-box.tsx
@@ -1,23 +1,28 @@
 import { getTranslations } from 'next-intl/server';
-import { ChevronLeft, Link } from 'lucide-react';
+import { ChevronLeft, Link as LinkIcon } from 'lucide-react';
 
 import { AnimatedShinyText } from '@/components/magicui/animated-shiny-text';
+import { Link } from '@/i18n/navigation';
 
 const LinkBox = async () => {
   const t = await getTranslations('HomePage');
 
   return (
-    <div className="group border-primary/30 rounded-full border sm:max-w-[332px]">
+    <Link
+      href="https://cloud.muthhelerp.com/"
+      className="group border-primary/30 rounded-full border sm:max-w-[332px]"
+    >
       <AnimatedShinyText className="flex-center hover:text-foreground hover:dark:text-foreground px-4 py-2 transition ease-out">
         <span className="flex-horizontally gap-x-1.5 text-base">
-          <Link className="text-primary" size={18} /> {t('heroSection.linkBox')}
+          <LinkIcon className="text-primary" size={18} />{' '}
+          {t('heroSection.linkBox')}
         </span>
         <ChevronLeft
           size={18}
           className="trans-transform ease-in-out group-hover:-translate-x-1"
         />
       </AnimatedShinyText>
-    </div>
+    </Link>
   );
 };
 
